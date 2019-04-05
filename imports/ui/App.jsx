92be49542fb0418c6024b0190cf007030a7d68c6
sososummer88 <summerhong1@gmail.com>
2019-04-04 19:08:56 -0700

    yPage

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,11 +5,8 @@ import Homepage from "./pages/Homepage.jsx";
 import Recipe from "./pages/Recipe.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
-<<<<<<< HEAD
 import MyPage from "./pages/MyPage.jsx";
-=======
 import ChefPage from "./pages/ChefPage.jsx";
->>>>>>> 5d070ae697b567f3498fe1e602ccb7bba41ff6b4
 
 const App = () => (
 	<div>
